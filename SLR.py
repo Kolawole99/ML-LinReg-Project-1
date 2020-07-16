226df868e7231bc10cc89d5445cd6574f0be3059
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-16 04:24:30 +0100

    Data exploration for detailed analysis

--- SLR.py
+++ SLR.py
@@ -20,6 +20,30 @@ import numpy as np
 # FUEL CONSUMPTION COMB (L/100 km) e.g. 9.2
 # CO2 EMISSIONS (g/km) e.g. 182 --> low --> 0
 
-df = pd.read_csv("FuelConsumption.csv")
+df = pd.read_csv('FuelConsumptionCo2.csv')
 # take a look at the dataset
 print(df.head())
+
+
+
+#====================================DATA EXPLORATION===============================
+# summarize the data
+df.describe()
+#select some features to explore more.
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+#we can plot each of these features
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+viz.hist()
+plt.show()
+
+#Now, lets plot each of these features vs the Emission, to see how linear is their relation
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
